8e3d4e93409ba2bdb499074e035f07e5dffa2bb3
Mickey <Micky40086@gmail.com>
2020-07-20 16:49:30 +0800

    feat: set sass module

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import styles from './index.scss'
+
+export default function Index() {
+  return (
+    <div className={styles.container}>
+      <div>
+        <p>Hello World</p>
+      </div>
+    </div>
+  )
+}
