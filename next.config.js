78ab96dc4df7f53852d7918301d9a85c562c592d
Mickey <micky40086@gmail.com>
2020-08-05 13:52:00 +0800

    feat: update next version to 9.5.1 and remove alias config from webpack because tsconfig will handle that

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 const path = require('path')
 
 module.exports = {
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.resolve.alias['~'] = path.resolve(__dirname, 'src')
-    config.resolve.alias['components'] = path.resolve(
-      __dirname,
-      'src',
-      'components'
-    )
-
-    return config
-  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'src', 'styles')],
   },
