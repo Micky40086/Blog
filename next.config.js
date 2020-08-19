b0bb732c17670d062ec908074452a5a1c377ea0b
Mickey <micky40086@gmail.com>
2020-08-20 00:56:13 +0800

    feat: use @zeit/next-sass config getCssModuleLocalIdent

--- next.config.js
+++ next.config.js
@@ -1,7 +1,51 @@
 const path = require('path')
+const withSass = require('@zeit/next-sass')
+const loaderUtils = require('loader-utils')
 
-module.exports = {
-  sassOptions: {
+module.exports = withSass({
+  cssModules: true,
+  cssLoaderOptions: {
+    importLoaders: 1,
+    getLocalIdent: (loaderContext, localIdentName, localName, options) => {
+      return getCssModuleLocalIdent(
+        loaderContext,
+        localIdentName,
+        localName,
+        options
+      )
+    },
+  },
+  sassLoaderOptions: {
     includePaths: [path.join(__dirname, 'src', 'styles')],
   },
+})
+
+// next.js getLocalIdent source code
+const regexLikeIndexModule = /(?<!pages[\\/])index\.module\.(scss|sass|css)$/
+
+function getCssModuleLocalIdent(context, _, exportName, options) {
+  const relativePath = path
+    .relative(context.rootContext, context.resourcePath)
+    .replace(/\\+/g, '/')
+
+  const fileNameOrFolder = regexLikeIndexModule.test(relativePath)
+    ? '[folder]'
+    : '[name]'
+
+  const hash = loaderUtils.getHashDigest(
+    Buffer.from(`filePath:${relativePath}#className:${exportName}`),
+    'md5',
+    'base64',
+    5
+  )
+
+  return loaderUtils
+    .interpolateName(
+      context,
+      fileNameOrFolder + '_' + exportName + '__' + hash,
+      options
+    )
+    .replace(/\.module_/, '_')
+    .replace(/[^a-zA-Z0-9-_]/g, '_')
+    .replace(/^(\d|--|-\d)/, '__$1')
 }
